6322567be5a5da9831ce70b9a51d373a35556c8d
SUDIP MAHATO <mahatosudip837@gmail.com>
2023-09-22 20:06:35 +0530

    update

--- app.js
+++ app.js
@@ -19,5 +19,40 @@ $(document).ready(function(){
         $(this).find(".dropdown-content").stop(true, true).slideUp("fast");
     });
 
+    $('.testimonial').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+    $('.sponser').owlCarousel({
+        loop:true,
+        margin:20,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+
 
 })
