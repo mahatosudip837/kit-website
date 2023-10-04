cf66e9c5ac54380e3b48e881a52d255678b43831
SUDIP MAHATO <mahatosudip837@gmail.com>
2023-10-04 20:05:41 +0530

    update

--- app.js
+++ app.js
@@ -133,6 +133,17 @@ $(document).ready(function(){
     });
 
 
+    $(".animated-progress span").each(function () {
+        $(this).animate(
+          {
+            width: $(this).attr("data-progress") + "%",
+          },
+          1000
+        );
+        // $(this).text($(this).attr("data-progress") + "%");
+      });
+
+
     
 
 
